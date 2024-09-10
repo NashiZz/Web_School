ba6e3c97586d348b3881d2edc448c07638f0738f
Nashi <115577029+NashiZz@users.noreply.github.com>
2024-09-10 15:54:59 +0700

    Revert "teacher_page"
    This reverts commit d9c04246af24cef7cc2b315d1dbac4d7835fd2a7.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import HomePage from "./component/pages/Home/home_page";
 import Root_page from "./component/pages/Root_Page/root_page";
 import SchoolHistory from "./component/pages/School_History/School_History";
-import People from "./component/pages/People_Page/people_page";
 
 const routers = createBrowserRouter([
   {
@@ -16,10 +15,6 @@ const routers = createBrowserRouter([
       {
         path: "/school_history",
         element: <SchoolHistory />
-      },
-      {
-        path: "/people",
-        element: <People />
       },
     ],
   }
--- src/component/header_footer/Header.tsx
+++ src/component/header_footer/Header.tsx
@@ -100,7 +100,7 @@ function Header() {
                 className="absolute left-0 mt-2 bg-white border border-gray-300 rounded-md shadow-lg w-48 z-20"
                 onMouseLeave={handleDropdownMouseLeave}
               >
-                <Link to="/people" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">กลุ่มสาระ ภาษาไทย</Link>
+                <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">กลุ่มสาระ ภาษาไทย</a>
                 <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">นักเรียน</a>
               </div>
             )}
